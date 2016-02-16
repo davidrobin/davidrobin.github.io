0ce7e777f8e60e084b817bf83aed63f9ab645f83
David ROBIN <darobin@gmail.com>
2016-02-16 16:25:04 +0100

    New lazy load page

--- lazy-load/lazy-1.0.0.function.js
+++ lazy-load/lazy-1.0.0.function.js
@@ -2,60 +2,54 @@ function lazy(selector) {
 
   var pixelDensity = 1.5;
 
-  var i = 0;
-
   var lazyElements = document.querySelectorAll(selector);
+
+  var lazyFadeElements = document.querySelectorAll(selector + '.fade');
+
+
+  /* 
+  FUNCTIONS
+  */
 
   function loadBG(element, url) {
 
-    if (element.classList.contains('fade')) {
+    element.style.backgroundImage = "url('" + url + "')";
 
-      var image = document.createElement('img');
-
-      image.style.position= 'absolute';
-
-      image.style.top= '-10000px';
-
-      document.querySelector('body').appendChild(image);
-
-      image.src = url;
-
-      image.addEventListener('load', function() {
-
-        element.classList.add('fade-overlay');
-
-        element.style.backgroundImage = "url('" + url + "')";
-
-        this.style.display = 'none';
-
-      });
-
-    }
-
-    else {
-
-      element.style.backgroundImage = "url('" + url + "')";
-
-    }
-
-  }
-
+  } // End of loadBG Function
 
   function loadIMG(element, url) {
 
     element.src= url;
 
-    if (element.classList.contains('fade')) {
+  } // End of loadIMG Function
 
-      element.addEventListener('load', function() {
-        
-          element.classList.toggle('fade-img');
+  function loadFadeBG(element, url) {
 
-      });
+    var image = document.createElement('img');
 
-    }
+    image.style.position= 'absolute';
 
-  }
+    image.style.top= '-10000px';
+
+    document.querySelector('body').appendChild(image);
+
+    image.src = url;
+
+    image.addEventListener('load', function() {
+
+      element.classList.add('fade-overlay');
+
+      this.style.display = 'none';
+
+    });
+
+  } // End of loadFadeBG Function
+
+  function loadFadeIMG(element) {
+
+    element.classList.add('fade-img');
+
+  } // End of loadFadeIMG Function
 
   // IF one or more Elements are found
   if ( lazyElements.length > 0 ) {
@@ -109,14 +103,74 @@ function lazy(selector) {
 
       }
 
-      else {
-
-        return false;
-
-      }
-
     } // End of LOOP for
 
   } // End of IF LazyElement found
+
+  function fadeImg() {
+
+    var scrollPos = document.body.scrollTop;
+
+      // IF one or more Elements are found
+      if ( lazyFadeElements.length > 0 ) {
+
+        var lazyFadeElement = '';
+
+        for ( i=0; i < lazyFadeElements.length; i++ ) {
+
+          // Define current Lazy Element
+          lazyFadeElement = lazyFadeElements[i];
+
+          if ( lazyFadeElement.getBoundingClientRect().top <= window.innerHeight - 50 && !lazyFadeElement.classList.contains('fade-show')) {
+
+          // Images
+          if ( lazyFadeElement.getAttribute("data-src-2x") && window.devicePixelRatio >= pixelDensity || lazyFadeElement.getAttribute("data-src") ) {
+
+            loadFadeIMG(lazyFadeElement);
+            lazyFadeElement.classList.add('fade-show');
+
+          }
+
+          // Background Images with BACKGROUND-IMAGE: url('') attribute
+          else if ( lazyFadeElement.getAttribute("data-bg-2x") && window.devicePixelRatio >= pixelDensity || lazyFadeElement.getAttribute("data-bg") ) {
+
+            var url = lazyFadeElement.getAttribute("data-bg-2x") || lazyFadeElement.getAttribute("data-bg");
+
+            loadFadeBG(lazyFadeElement, url);
+            lazyFadeElement.classList.add('fade-show');
+
+          }
+
+        } // End of IF Scroll >= LazyFadeElement Top
+
+        } // End of LOOP for
+
+      } // End of IF LazyFadeElement found
+
+  } // End of fadeImg Function
+
+  var scrollActiv = false;
+
+  window.addEventListener('scroll', function() {
+
+    if (!scrollActiv) {
+
+      scrollActiv = true;
+
+      fadeImg();
+
+      setTimeout(function() { 
+
+        scrollActiv = false;
+
+      }, 
+
+      100); 
+
+    } // End of IF ScrollACtiv is false
+
+}); // End of SCROLL Function
+
+  setTimeout(fadeImg, 250);
 
 } // End of LAZY Function
